bb71f9ae2d31e679b6f2b054a17132685517741f
SahanDhanujaya <sahandhanujaya18@gmail.com>
2025-10-04 09:23:34 +0530

    tailwind css configured

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+function HomePage() {
+  return (
+    <div>
+        {/* profile image  */}
+        <div>
+            <img src="" alt="my profile" />
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
