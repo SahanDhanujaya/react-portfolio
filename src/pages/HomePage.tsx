2ba92c763c8f6eceba61d191c85ba34166178db2
SahanDhanujaya <sahandhanujaya18@gmail.com>
2025-10-04 10:58:42 +0530

    make react app suitable to run

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,14 @@ function HomePage() {
   return (
     <div>
         {/* profile image  */}
-        <div>
-            <img src="" alt="my profile" />
+        <div className="flex justify-center flex-col items-center h-screen">
+            <img src="" alt="my profile"
+                className="w-48 h-48 rounded-full mx-auto border-4 border-black"
+             />
+             <div className="m-4">
+                <h1 className="text-3xl text-black font-bold">Sahan Dhanujaya</h1>
+                <h3 className="text-lg text-black">Profecional Software Developer</h3>
+             </div>
         </div>
     </div>
   )
